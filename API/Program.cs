edddbbd8c048c5ae56d770db2ce8de2e997385dd
hilalkabanli <hilal.kabanlii@gmail.com>
2023-12-10 20:41:24 +0300

    seeding the data

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +26,18 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+var scope=app.Services.CreateScope();
+var context=scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger=scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex,"An error occured during migration");
+}
 
 app.Run();
