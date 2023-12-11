658bc84a1d1002fb89b807516ac40c12b266d90f
hilalkabanli <hilal.kabanlii@gmail.com>
2023-12-11 16:36:10 +0300

    async way

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -12,22 +13,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly StoreContext context;
+        
+        private readonly StoreContext _context;
         
         public ProductController(StoreContext context)
         {
-            this.context = context;
+            _context = context;
         }
         
         [HttpGet]
-        public ActionResult<List<Product>> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
-            var products= context.Products.ToList();
-            return Ok(products);
+           return await _context.Products.ToListAsync();
         }
+
         [HttpGet("{id}")] //api/products/3
-        public ActionResult<Product> GetProduct(int id){
-            return context.Products.Find(id);
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _context.Products.FindAsync(id);
         }
     }
 }
