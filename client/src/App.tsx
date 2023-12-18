0f8d6a1d408e76ea13a44a84023036014a96ba3f
hilalkabanli <hilal.kabanlii@gmail.com>
2023-12-18 17:25:06 +0300

    adding typescript interface to product

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import {useState,useEffect} from "react"
+import { Product } from "./product";
 
 function App() {
-  const [products, setProducts] = useState(
-    [
-      {name: 'Product 1', price: 100},
-      {name: 'Product 2', price: 200},
-      {name: 'Product 3', price: 300},
-    ]
+  const [products, setProducts] = useState<Product[]>(
+    []
   );
   useEffect(() => {
     console.log(products)
@@ -18,19 +15,23 @@ function App() {
   },[]) 
 
   function addProduct(){  
-    setProducts(
-      prevState => 
-      [...prevState, 
-      {name: 'Product '+(prevState.length+1), price: (prevState.length*100)+100}
-    ])
+    setProducts( prevState =>[...prevState, 
+      {
+        id: prevState.length + 101,
+        name: 'product ' + (prevState.length + 1),
+        price: (prevState.length * 100)+100,
+        brand: 'some brand',
+        description: 'some description',
+        pictureUrl: 'http://picsum.photos/200'
+      }])
   }
     
   return (
     <div className="app">
       <h1>Re-Store</h1>
       <ul>
-        {products.map((item, index)=>(
-          <li key={index}>{item.name} - {item.price}</li>
+        {products.map(product=>(
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}       
       </ul>
       <button onClick={addProduct}>Add Product</button>
